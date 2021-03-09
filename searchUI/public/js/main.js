2e2f3354ecc70183c030365332c5b4d387336304
GleamingStar <tjdduddl10@gmail.com>
2021-03-09 17:23:52 +0900

    Feat: add change, hide, updown event, render() add changeEvent() that get recommend suggestion add hideEvent() that activate when click outside of search bar add updownEvent() to select the item of recommend suggestion add render() to fill recommend suggestion by fetch amazon.com

--- searchUI/public/js/main.js
+++ searchUI/public/js/main.js
@@ -1,6 +1,6 @@
 import { _ } from './util.js'
-import { Slide } from './slide.js'
-import { ImageLoader } from './image_loader.js'
+// import { Slide } from './slide.js'
+// import { ImageLoader } from './image_loader.js'
 import { Search } from './search.js'
 
 const selectors = {
@@ -12,7 +12,9 @@ const selectors = {
     seeMore: _.$('.main__see-more-button'),
     secondContent: _.$('.main__2nd-content'),
     searchBar: _.$('.header__search-bar'),
-    suggestion: _.$('.search-bar__suggestion'),
+    searchInput: _.$('.search-bar__input'),
+    suggestionHot: _.$('.search-bar__suggestion-hot'),
+    suggestionRecommend: _.$('.search-bar__suggestion-recommend'),
     rollItems: _.$('.search-bar__roll-items')
 }
 new Search(selectors)
