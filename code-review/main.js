91f5cc358e055d5203955350b86b62e529aeef93
Seong Young Jeong <70461368+GleamingStar@users.noreply.github.com>
2021-03-09 22:20:55 +0900

    [Seong] searchUI-step2 (#17)
    * Feat: add search.js for search bar UI
    
    set fundamental HTML structure by copy week 3 repository
    
    add roll() to roll item, clickEvent() to reveal suggestion item
    
    * Feat: add change, hide, updown event, render()
    add changeEvent() that get recommend suggestion
    add hideEvent() that activate when click outside of search bar
    add updownEvent() to select the item of recommend suggestion
    add render() to fill recommend suggestion by fetch amazon.com
    
    * Refactor: refactor search.js
    
    apply prettier to .js, rename changeEvent(), replace .then to async/await

--- code-review/main.js
+++ code-review/main.js
@@ -1,21 +1,23 @@
-import { _ } from './util.js'
-import { Slide } from './slide.js'
-import { ImageLoader } from './image_loader.js'
-import { Search } from './search.js'
+import { _ } from "./util.js";
+// import { Slide } from './slide.js'
+// import { ImageLoader } from './image_loader.js'
+import { Search } from "./search.js";
 
 const selectors = {
-    wrapper: _.$('.slide--wrapper'),
-    slideItems: _.$A('.slide--item'),
-    leftButton: _.$('.left-button'),
-    rightButton: _.$('.right-button'),
-    scrollButtons: _.$A('.scroll-button'),
-    seeMore: _.$('.main__see-more-button'),
-    secondContent: _.$('.main__2nd-content'),
-    searchBar: _.$('.header__search-bar'),
-    suggestion: _.$('.search-bar__suggestion'),
-    rollItems: _.$('.search-bar__roll-items')
-}
-new Search(selectors)
+	wrapper: _.$(".slide--wrapper"),
+	slideItems: _.$A(".slide--item"),
+	leftButton: _.$(".left-button"),
+	rightButton: _.$(".right-button"),
+	scrollButtons: _.$A(".scroll-button"),
+	seeMore: _.$(".main__see-more-button"),
+	secondContent: _.$(".main__2nd-content"),
+	searchBar: _.$(".header__search-bar"),
+	searchInput: _.$(".search-bar__input"),
+	suggestionHot: _.$(".search-bar__suggestion-hot"),
+	suggestionRecommend: _.$(".search-bar__suggestion-recommend"),
+	rollItems: _.$(".search-bar__roll-items"),
+};
+new Search(selectors);
 
 // fetch("http://localhost:3000/planningEvent.json")
 //     .then(response => response.json())
