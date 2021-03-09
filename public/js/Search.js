a3e645a0a3b8a5c77fd9e3295420e481a3d569bb
SEONKYU\difke <somedaycode@gmail.com>
2021-03-10 02:52:36 +0900

    add: debounce를 이용한 inputHandler 추가

--- public/js/Search.js
+++ public/js/Search.js
@@ -5,9 +5,10 @@ export function Search() {
   this.searchInput = _.$('.search-bar');
   this.suggestion = _.$('.search-suggestion');
   this.keywordsToroll = _.$('.nav-search-rank');
+  this.rollKeyword = _.$('.search-rollkeywords');
   this.moveY = -2.5;
   this.timer;
-  this.rollKeyword = _.$('.search-rollkeywords');
+  this.debouncer;
 }
 
 Search.prototype.init = function () {
@@ -18,6 +19,7 @@ Search.prototype.init = function () {
 Search.prototype.onEvents = function () {
   _.on(this.searchInput, 'focus', this.focusHandler.bind(this));
   _.on(this.searchForm, 'focusout', this.focusoutHandler.bind(this));
+  _.on(this.searchInput, 'input', this.inputHandler.bind(this));
 };
 
 Search.prototype.focusHandler = function ({ target }) {
@@ -31,7 +33,23 @@ Search.prototype.focusoutHandler = function ({ target }) {
   if (this.isNotTarget(target)) return;
   _.rmClass(this.suggestion, 'suggestion-visible');
   this.runKeyWordsRoll();
-  this.showRollKeyword();
+  if (!this.searchInput.value) {
+    this.showRollKeyword();
+  }
+};
+
+Search.prototype.inputHandler = function ({ target }) {
+  const ms = 1000;
+  const text = target.value;
+  this.debounce(() => this.fetchKeywords(text), ms);
+};
+
+Search.prototype.debounce = function (fn, ms) {
+  let timer = this.debouncer;
+  if (timer) clearTimeout(timer);
+  this.debouncer = setTimeout(() => {
+    fn();
+  }, ms);
 };
 
 Search.prototype.isNotTarget = function (target) {
@@ -73,15 +91,9 @@ Search.prototype.showRollKeyword = function () {
   _.rmClass(this.rollKeyword, 'search-rollkeyword-hidden');
 };
 
-Search.prototype.fetchKeywords = async function () {
-  await fetch(
-    `https://completion.amazon.com/api/2017/suggestions?session-id=141-6040242-7044009&customer-id=&request-id=7ZD2PSMEE2JF3CVEXGZF&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=undefined&prefix=${value}&event=onFocusEmptySearchTerm&limit=11&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1615301215702`,
-    {
-      headers: {
-        accept: 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cache-control': 'no-cache',
-      },
-    }
-  );
+Search.prototype.fetchKeywords = async function (text) {
+  const test = await fetch(
+    `https://completion.amazon.com/api/2017/suggestions?session-id=141-6040242-7044009&customer-id=&request-id=7ZD2PSMEE2JF3CVEXGZF&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=81&prefix=${text}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1615307516261`
+  ).then((res) => res.json());
+  console.log(test);
 };
