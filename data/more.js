df28ed376ae3381dce5fc13ba01952bfda2b066a
Hoyoung Son <xzxking17@gmail.com>
2021-03-09 22:20:59 +0900

    [Rano] Search UI step1 (#21)
    * Rebase to 7094f2a
    - Combined some refactored changes in the last quarter of Week 3.
    
    initial commit
    - index.ejs 초기 구조 작성
    
    Fix index.ejs (Making style...)
    
    Add 4 scss files
    
    Fix Dir structure change & Creating header style..
    
    Add data files & Fix content style
    1) 더보기 기능 관련 데이터 파일 추가
    2) content 부분 style 작업중
    
    [Add] data & data router, [Refactor] Dir Structure
    
    Add Data Control files
    1) 더보기 버튼 데이터 관련하여 fetch API를 활용하여 작업중에 있습니다.
    
    Fix Working on data processing..1 (moreBtn...ㅠㅠ)
    
    Fix Working on data processing..2 (moreData)
    
    Fix 더보기버튼 값 업데이트시, 값이 최대값 이상으로 올라가는 버그 수정..1
    
    [Fix] 상단 캐러셀 작업 중 (1)
    
    [Fix] 상단 캐러셀 작업 중 (2)
    
    [Refector & Fix] IndexControl.js
    
    [Fix] content_main_carousel btnEvent complete
    
    [Simplify] Change async-await to then-catch
    
    [Fix] Add event (main carousel pagging span)
    
    [Fix] index page: main carousel transform change
    - style: Change the value of Transform from translate3d to translateX
    
    [Fix] Bottom carousel style change
    - 뭔가 이상해서 엄청 삽질하다가 이제서야 수정완료
    
    [Fix] Bottom chrousel event complete. (test only)
    
    [Fix] Bottom chrousel event complete. (test only..2)
    
    [Add] hotCarousel data get method
    
    [Fix] DataManager.js
    
    [Fix] Press the button for 2 seconds to move by 2.
    - for Hot Carousel (Bottom Carousel)
    
    [Fix] main BestItem & Carousel, getData & render
    
    [Refector] refactoring index pages (1)
    
    [Refector] refactoring index pages (2)
    
    [Rename] IndexControl.js -> IndexController.js
    
    [Refector] All .scss files
    
    [Refector] Refactored based on the last branch of week 3
    
    * [Add] SearchUI style
    
    Co-authored-by: crong <crongro@gmail.com>

---
+++ data/more.js
@@ -0,0 +1,9 @@
+import more1 from './more/more1.json';
+import more2 from './more/more2.json';
+import more3 from './more/more3.json';
+import more4 from './more/more4.json';
+import more5 from './more/more5.json';
+
+const moreData = [...more1.contents, ...more2.contents, ...more3.contents, ...more4.contents, ...more5.contents];
+
+export default moreData;
---
+++ public/js/index.js
@@ -0,0 +1,44 @@
+import _ from './util.js';
+import IndexController from './index/IndexController.js';
+import DataManager from './index/DataManager.js';
+import SearchController from "./index/SearchController.js";
+
+const serverInfo = {serverURL: 'http://localhost:3001'};
+const dataManager = new DataManager(serverInfo);
+
+const indexWrappers = {
+    mainBestWrapper: _.$('.content__main__one'),
+    mainCarouselWrapper: _.$('.content__main__carousel'),
+    moreWrapper: _.$('.content__more'),
+    hotCarouselWrapper: _.$('.content__hot__carousel')
+};
+
+const  {mainBestWrapper, mainCarouselWrapper, moreWrapper, hotCarouselWrapper} = indexWrappers;
+const controlItems = {
+    mainSlideItems: _.$All('.slide > div', mainCarouselWrapper),
+    mainSlidePagingInnerList: _.$All('.paging__inner > span', mainCarouselWrapper),
+    mainBestItemImg: _.$('a > img', mainBestWrapper),    
+    moreViewBtn: _.$('.content__more__btn', moreWrapper),
+    moreViewFrame: _.$('ul',  _.$('.content__more__view', moreWrapper) ),
+    hotSlideItems: _.$All('ul > li', hotCarouselWrapper),
+};
+
+const options = {
+    mainCarouselAnimateInterval: 5000,
+    mainCarouselAnimateDirection: 'next',
+    mainCarouselTransitionDuration: '0.4s',
+    hotCarouselTransitionDuration: '0.4s',
+};
+
+new IndexController(dataManager, indexWrappers, controlItems, options).init();
+
+new SearchController(_.$('.search')).init();
+
+// indexController에 넣던지.. 분리하기 -----------------------------------------------
+_.addEvent(_.$('.all-wrapper'), 'mouseover', ({target}) => {
+    const closestTarget = _.closestSelector(target, '.search');
+
+    if (!closestTarget)
+        _.classToggleForce(_.$('.search .search__suggestion'), 'visibility--hidden', true);
+});
+// ---------------------------------------------------------------------------------
