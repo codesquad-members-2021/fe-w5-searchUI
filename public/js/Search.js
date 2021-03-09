fe88d05c7914b3b3db10377bf2e917a0f0be3baa
SEONKYU\difke <somedaycode@gmail.com>
2021-03-09 21:59:16 +0900

    add: Search class 구현
    검색창을 제어하는 클래스를 프로토타입으로 구현했다.
    onEvents 프로토타입 함수를 통해 foucs와 foucsout 이벤트를 실행한다.
    검색창을 눌렀을 때 인기검색어 목록이 나온다.
    인기검색어 목록은 검색창에서도 rolling되는 애니메이션이 발생한다.

--- public/js/Search.js
+++ public/js/Search.js
@@ -1,20 +1,23 @@
-import { _, delay } from './util.js';
+import { _ } from './util.js';
 
 export function Search() {
-  this.searchBar = _.$('.nav-search-bar');
+  this.searchForm = _.$('.nav-search-bar');
+  this.searchInput = _.$('.search-bar');
   this.suggestion = _.$('.search-suggestion');
   this.keywordsToroll = _.$('.nav-search-rank');
   this.moveY = -2.5;
+  this.timer;
 }
 
 Search.prototype.onEvents = function () {
-  _.on(this.searchBar, 'click', this.clickHandler.bind(this));
-  _.on(this.searchBar, 'focusout', this.focusoutHandler.bind(this));
+  _.on(this.searchInput, 'focus', this.focusHandler.bind(this));
+  _.on(this.searchForm, 'focusout', this.focusoutHandler.bind(this));
 };
 
-Search.prototype.clickHandler = function ({ target }) {
+Search.prototype.focusHandler = function ({ target }) {
   if (this.isNotTarget(target)) return;
   _.addClass(this.suggestion, 'suggestion-visible');
+  clearInterval(this.timer);
 };
 
 Search.prototype.focusoutHandler = function ({ target }) {
@@ -29,19 +32,25 @@ Search.prototype.isNotTarget = function (target) {
 Search.prototype.animateKeywords = function (move, start, end) {
   this.keywordsToroll.style.transform = `translate3d(0,${move}rem,0)`;
   this.keywordsToroll.ontransitionend = () => {
-    if (this.moveY !== end) return;
-    this.keywordsToroll.style.transitionDuration = '0ms';
-    this.keywordsToroll.style.transform = 'translate3d(0, 0, 0)';
-    this.moveY = start;
+    if (this.moveY === end) {
+      this.keywordsToroll.style.transitionDuration = '0ms';
+      this.keywordsToroll.style.transform = 'translate3d(0, 0, 0)';
+      this.moveY = start;
+    }
   };
   this.keywordsToroll.style.transitionDuration = '700ms';
 };
 
-Search.prototype.runKeywordsRoll = async function () {
+Search.prototype.runKeyWordsRoll = function () {
   const startMove = -2.5;
   const endMove = -27.5;
-  const runRoll = this.animateKeywords(this.moveY, startMove, endMove);
-  await delay(runRoll, 1000);
-  this.runKeywordsRoll();
-  this.moveY += -2.5;
+  //   const runRoll = this.animateKeywords(this.moveY, startMove, endMove);
+  this.timer = setInterval(() => {
+    this.animateKeywords(this.moveY, startMove, endMove);
+    this.moveY += -2.5;
+  }, 1000);
+  //   await delay(runRoll, 1000);
+
+  //   this.runKeyWordsRoll();
+  //   this.moveY += -2.5;
 };
