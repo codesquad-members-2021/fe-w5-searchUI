2826e8b8bcf44943989414b169fa7f256c698105
swing-park <ongsim123@gmail.com>
2021-03-10 22:13:22 +0900

    Add : focus시 Rolling stop기능 추가

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,20 @@
+import 'regenerator-runtime/runtime';
 import { _, delay } from "./util"
 import CarouselUI from "./carouselUI";
 import { carouselUIManager } from "./carouselUIManager";
 import RollingUI from "./rollingUI";
 import RollingUIManager from "./rollingUIManager";
+import { test } from "./searchDropdownUI";
 
 const init = () => {
-    new CarouselUI().insertDOM();
-    setTimeout(() => carouselUIManager(_.$All('.carouselUI--img')), 300);
-    new RollingUI(_.$("#rolledList")).init();
-    new RollingUIManager().rollingList();
+    const carouselUI = new CarouselUI(_.$('.carouselUI'));
+    const rollingUI = new RollingUI(_.$("#rolledList"));
+    const rollingUIManager = new RollingUIManager(_.$("#rolledList"));
+
+    carouselUI.insertDom();
+    setTimeout(() => carouselUIManager(_.$All(".carouselUI--img")), 300)
+    rollingUI.init();
+    rollingUIManager.eventHandler(_.$("#rollingUI--input"));
+    rollingUIManager.moveRollingList();
 };
 init();
--- public/js/rollingUIManager.js
+++ public/js/rollingUIManager.js
@@ -1,26 +1,53 @@
 import { _ } from "./util";
 
 export default class RollingUIManager {
-    constructor() {
-        this.el = _.$("#rolledList");
-        this.Y = -10;
+    constructor(el) {
+        this.el = el;
+        this.defaultY = -10;
+        this.moveY = this.defaultY;
+        this.listsNum = 10;
+        this.listPixel = 30;
+        this.transitionMs = 700;
+        this.intervalTime = 2000;
     }
 }
 
-RollingUIManager.prototype.rollingList = function () {
-    this.el.ontransitionend = () => {
-        if (this.Y == -310) {
+RollingUIManager.prototype.moveRollingList = function () {
+    this.el.addEventListener("transitionend", () => {
+        if (this.moveY == this.defaultY - (this.listPixel * this.listsNum)) {
             this.el.style.transition = "0ms";
-            this.Y = -10;
-            this.el.style.transform = `translateY(${this.Y}px)`;
+            this.moveY = this.defaultY;
+            this.el.style.transform = `translateY(${this.moveY}px)`;
         }
-    }
-    this.el.style.transition = "700ms";
-    this.el.style.transform = `translateY(${this.Y}px)`;
+    })
 
-    setInterval(() => {
-        this.Y -= 30;
-        this.el.style.transition = "700ms";
-        this.el.style.transform = `translateY(${this.Y}px)`;
-    }, 1000)
+    this.el.style.transition = `${this.transitionMs}ms`
+    this.el.style.transform = `translateY(${this.moveY}px)`;
+
+    this.timer = setInterval(() => {
+        this.moveY -= this.listPixel;
+        this.el.style.transition = `${this.transitionMs}ms`
+        this.el.style.transform = `translateY(${this.moveY}px)`;
+    }, this.intervalTime)
+}
+
+RollingUIManager.prototype.stopRollingList = function () {
+    clearInterval(this.timer);
+};
+
+RollingUIManager.prototype.eventHandler = function (el) {
+    _.on(el, "focus", this.focusHandler.bind(this));
+    _.on(el, "blur", this.blurHandler.bind(this));
+};
+
+RollingUIManager.prototype.focusHandler = function () {
+    this.stopRollingList();
+    this.el.style.transition = "0ms";
+    this.el.style.opacity = "0";
+};
+
+RollingUIManager.prototype.blurHandler = function () {
+    this.moveRollingList();
+    this.el.style.transition = "0ms";
+    this.el.style.opacity = "1";
 }
