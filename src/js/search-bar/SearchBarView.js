df8ae5d31238bfafbfb66f2a20c4b2578eb87bbe
cchoongh <cndgh49@naver.com>
2021-03-10 17:19:15 +0900

    Refactor SearchBarView

--- src/js/search-bar/SearchBarView.js
+++ src/js/search-bar/SearchBarView.js
@@ -1,19 +1,45 @@
 import '../../scss/SearchBarView.scss';
 import { _ } from '../util.js';
 
-export function SearchBarView() {
+export function SearchBarView({ carouselView, suggestionView } = {}) {
   this.$target;
+  this.$input;
+  this.$button;
+  this.carauselView = carouselView;
+  this.suggestionView = suggestionView;
 
-  this.createEl = () => {
-    return (this.$target = _.genEl('DIV', {
-      classNames: ['search-bar'],
-      template: this.template,
-    }));
+  this.init = function () {
+    this.$target = this.createEl();
+    this.$input = _.$('.search-bar__input', this.$target);
+    this.$button = _.$('.search-bar__btn', this.$target);
+    this.onEvents();
   };
 
-  this.getTargetEl = () => {
+  this.createEl = function () {
+    return _.genEl('DIV', {
+      classNames: ['search-bar'],
+      template: this.template(),
+    });
+  };
+
+  this.getEl = function () {
     return this.$target;
   };
+
+  this.onEvents = function () {
+    this.$input.addEventListener('change', this.onChangeInput.bind(this));
+  };
+
+  this.onChangeInput = function ({ target }) {
+    // TODO: trigger suggestion view
+  };
+
+  this.init();
 }
 
-SearchBarView.prototype.template = `<input type="text" class="search-bar__input"></input><div class="search-bar__btn-cont"><img class="search-bar__btn-cont__ic"/></div>`;
+SearchBarView.prototype.template = function () {
+  return `<input type="text" class="search-bar__input"></input>
+          <button class="search-bar__btn">
+            <img class="search-bar__btn__ic"/>
+          </button>`;
+};
