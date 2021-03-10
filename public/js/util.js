d7fe77a73ecd2961e9a65981ab20ef5390d955eb
swing-park <ongsim123@gmail.com>
2021-03-10 22:12:38 +0900

    Add & fix : eventListner 유틸 함수 추가 , delay 함수 수정

--- public/js/util.js
+++ public/js/util.js
@@ -1,11 +1,12 @@
 const _ = {
     $: (selector, base = document) => base.querySelector(selector),
-    $All: (selector, base = document) => base.querySelectorAll(selector)
+    $All: (selector, base = document) => base.querySelectorAll(selector),
+    on: (el, event, fn) => el.addEventListener(event, fn)
 };
 
 const delay = (fn, ms) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(fn), ms);
+        setTimeout(() => resolve(fn()), ms);
     })
 }
 
