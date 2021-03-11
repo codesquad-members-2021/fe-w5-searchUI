2e70bc351085c6593e9ad2875b3164619214208a
Juddroid <nobllizdc@gamil.com>
2021-03-12 04:34:06 +0900

    [SimilarWord] CORS 문제를 우회하여 script를 이용한 데이터 받아오기

--- raccoon-search-express/front/src/js/similarword.js
+++ raccoon-search-express/front/src/js/similarword.js
@@ -1,11 +1,19 @@
 import { _ } from './const';
+import FetchAPI from './fetchAPI';
 
 export default function Similarword() {
-  this.string = string;
   this.input = _.$('.search-bar--keyword');
+  this.fetchAPI = new FetchAPI();
 }
 
 Similarword.prototype.addEvent = function () {
-  this.input.addEventListener('keyup', requestData);
+  this.input.addEventListener('keyup', this.requestData.bind(this));
 };
-Similarword.prototype.requestData = function () {};
+
+Similarword.prototype.requestData = function () {
+  return this.fetchAPI.getSimilarword(this.input.value, similarword);
+};
+
+window['similarword'] = function similarword(data) {
+  return data;
+};
