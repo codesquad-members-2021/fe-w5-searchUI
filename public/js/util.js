6c05dda61bbb202dbb50e72e3ad977f50b6e3975
SEONKYU\difke <somedaycode@gmail.com>
2021-03-11 19:13:30 +0900

    add: util.js 예외처리 및 contains 함수 추가

--- public/js/util.js
+++ public/js/util.js
@@ -2,9 +2,10 @@ const _ = {
   $: (selector) => document.querySelector(selector),
   $All: (selector) => document.querySelectorAll(selector),
   on: (el, action, callback) => el.addEventListener(action, callback),
-  addClass: (el, className) => el.classList.add(className),
-  rmClass: (el, className) => el.classList.remove(className),
-  toggleClass: (el, className) => el.classList.toggle(className),
+  addClass: (el, className) => el?.classList.add(className),
+  rmClass: (el, className) => el?.classList.remove(className),
+  toggleClass: (el, className) => el?.classList.toggle(className),
+  contains: (el, className) => el?.classList.contains(className),
 };
 
 const delay = (data, time) => {
