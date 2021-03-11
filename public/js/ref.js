87e73a2895379be4ac0738c8234a6fd6d5c40289
조홍비 <60209518+dyongdi@users.noreply.github.com>
2021-03-11 22:13:57 +0900

    [Autumn] 검색창과 아래 팝업 칸 UI 만들기, fetch로 데이터 받아오기 (#51)
    * Add. 검색창 기본 스타일링
    
    * 데이터 fetch 연습
    
    * Fix. CORS error. url 안에서 줄바꿈을 한 게 문제였음
    
    * 모듈화를 위해 잠시 주석처리
    
    * Add. util 추가
    
    * Add. 인기 검색어 박스, 롤링 부분 기본 스타일링
    
    * Feat. 인기 검색어 데이터 불러와서 네모박스에 렌더링하는 함수 추가

---
+++ public/js/ref.js
@@ -0,0 +1,11 @@
+import { _ } from './util.js';
+
+const $search = _.$('.search-bar__input');
+const $rollingKeywords = _.$('.rolling-keywords');
+const $recommendedKeyword = _.$('.header__top__search-bar__sub-box');
+
+export {
+  $search,
+  $rollingKeywords,
+  $recommendedKeyword
+}
