5e7e88a315fdee4935119f12d4fbd174aa72acb5
Jaeuk-YOO <ryou9305@naver.com>
2021-03-12 06:58:00 +0900

    [add] node-fetch 라이브러리를 활용한 쇼핑하우로의 get, post 액션 추가

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import { default as apiRoutes } from "./apiRoutes.js";
 const app = express();
 const port = 3333;
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
