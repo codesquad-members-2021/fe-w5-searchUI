895cc3339292809f3c94971449ae7aad037e88da
Seong Young Jeong <70461368+GleamingStar@users.noreply.github.com>
2021-03-12 22:13:44 +0900

    Refactor: refactor search.js (#64)
    rename suggestion-hot/recommend to popular/autocomplete
    
    delete magic number in Search.rollLength, Search.rollHeight
    
    move hideSuggestions() event click on document to blur on input
    
    delete some week3 file

--- searchUI/app.js
+++ searchUI/app.js
@@ -1,12 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fs = require('fs');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -14,14 +10,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- searchUI/public/js/main.js
+++ searchUI/public/js/main.js
@@ -4,8 +4,8 @@ import { Search } from "./search.js";
 const selectors = {
 	searchBar: _.$(".header__search-bar"),
 	searchInput: _.$(".search-bar__input"),
-	suggestionHot: _.$(".search-bar__suggestion-hot"),
-	suggestionRecommend: _.$(".search-bar__suggestion-recommend"),
+	popular: _.$(".search-bar__popular"),
+	autocomplete: _.$(".search-bar__autocomplete"),
 	rollItems: _.$(".search-bar__roll-items"),
 };
 new Search(selectors);
