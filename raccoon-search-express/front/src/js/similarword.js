4f67a0dd21a1d85cad566ab6a221ae134d9094d1
Juddroid <nobllizdc@gamil.com>
2021-03-12 23:17:16 +0900

    [AutoSuggestion] suggestion이 없을 때, 예외처리

--- raccoon-search-express/front/src/js/similarword.js
+++ raccoon-search-express/front/src/js/similarword.js
@@ -4,8 +4,13 @@ import FetchAPI from './fetchAPI';
 export default function Similarword() {
   this.input = _.$('.search-bar--keyword');
   this.similarwordList = _.$('.list_similarwords');
+  this.wrapSuggestion = _.$('.wrap_suggestion');
   this.fetchAPI = new FetchAPI();
 }
+
+// 3. cursor를 위아래로 움직이면 css가 바뀐다.
+// 4. 1초 이상 아무 입력이 없으면 서버에서 데이터를 받아온다???
+// 추가미션. 최근 검색어 기능
 
 Similarword.prototype = {
   constructor: Similarword,
@@ -16,6 +21,10 @@ Similarword.prototype = {
     return this.fetchAPI.getSimilarword(this.input.value, similarword);
   },
   getGroupSuggestion: function (data) {
+    if (this.isEmptyData(data.items)) {
+      return (this.wrapSuggestion.style.display = 'none');
+    }
+    this.wrapSuggestion.style.display = 'block';
     const groupSuggestion = data.items.reduce((acc, cur) => {
       const inputKeyword = data.q;
       const suggestionKeyword = cur.slice(0, -2);
@@ -36,6 +45,10 @@ Similarword.prototype = {
   },
   drawGroupSuggestion: function (data) {
     this.similarwordList.innerHTML = `${this.getGroupSuggestion(data)}`;
+  },
+
+  isEmptyData: function (data) {
+    return data.length === 0;
   },
 };
 
