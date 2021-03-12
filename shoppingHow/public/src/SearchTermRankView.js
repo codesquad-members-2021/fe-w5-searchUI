e9ddc310170c59b52895ba2e7692be3d6a2f37f2
jeonyeonkyu <ehb6915@naver.com>
2021-03-12 22:13:46 +0900

    [Beemo] 자동완성 결과 + 방향키 조작 까지 구현 (#66)
    * Add: search input popupLayer 에 필요한 html, css 추가
    
    * Fix: SearchTermRankView에 넣는 rolling 데이터 따로 분리하여 추가
    
    * Feat: popupLayer 템플릿 추가
    
    * Feat: focusInputHandler, quitInputHandler 함수 구현
    
    * Fix: Search bar 검색어 키워드 입력시 영역 수정
    
    * Feat: 검색어 키워드 입력시 자동완성 결과 노출 구현
    
    디바운스, jsonp 활용
    
    * Fix: 검색어 입력시 아래 인기키워드 노출하기 수정
    
    * Feat: 인풋에 입력 후 인기검색어 아래, 위로 탐색하는 handler 구현
    
    * Fix: outline 제거
    
    * Fix: key가 ArrowDown이 아니거나 Up이 아닌경우 에러발생 제거

--- shoppingHow/public/src/SearchTermRankView.js
+++ shoppingHow/public/src/SearchTermRankView.js
@@ -1,45 +1,98 @@
-import fetch from 'cross-fetch';
 import { getResponseJsonUrl, delay } from './serviceUtil.js';
+import _ from './util.js';
 
-function SearchTermRankView({ $inputElement, $rankKeywordList, rankUrl }) {
+function SearchTermRankView({ $inputElement, $rankKeywordList, $searchWord, rankUrl, currentTypingUrl, rolling }) {
   this.$inputElement = $inputElement;
   this.$rankKeywordList = $rankKeywordList;
+  this.$searchWord = $searchWord;
   this.rankUrl = rankUrl;
+  this.currentTypingUrl = currentTypingUrl;
+  this.rolling = rolling;
+  this.typingTimer = null;
   this.rankKeywordArray = [];
-  this.rolling = {
-    timer: null,
-    timeSeconds: 3000,
-    maxPosition: '220',
-  };
+  this.currentTypingArray = [];
+  this.inputPopupClassName = 'input_popup';
+  this.RANKS_NUMBER = 10;
+  this.keyWordIndex = -1;
   this.init();
 }
 
 SearchTermRankView.prototype.init = async function () {
   const rankObj = await getResponseJsonUrl(this.rankUrl);
-  this.rankKeywordArray = rankObj.list.map(e => e.keyword).slice(0, 10);
-  console.log(this.$rankKeywordList);
-  this.makeTemplate();
+  this.rankKeywordArray = rankObj.list.map(item => item.keyword).slice(0, this.RANKS_NUMBER);
+  this.renderRollingTemplate();
+  this.renderPopupTemplate();
   this.rollingSearchBar();
   this.observeSearchBarState();
-  // this.initEvent();
+  this.initEvent();
+  window['putInResponseJsonpData'] = this.putInResponseJsonpData.bind(this);
 }
 
 SearchTermRankView.prototype.initEvent = function () {
-  this.inputElement.addEventListener('input', async ({ target }) => {
-    // const a = await getResponseJsonUrl((`https://suggest-bar.daum.net/suggest?callback=jQuery34109517340090479085_1615213153801&limit=10&mode=json&code=utf_in_out&q=${target.value}&id=shoppinghow_suggest`));
-    // console.log(a)
-  });
+  this.$inputElement.addEventListener('click', this.focusInputHandler.bind(this));
+  this.$inputElement.addEventListener('input', this.typingInputHandler.bind(this));
+  document.addEventListener('click', this.quitInputHandler.bind(this));
+  document.addEventListener('keydown', this.keydownInputHandler.bind(this));
 }
 
-SearchTermRankView.prototype.makeTemplate = function () {
-  let template = this.rankKeywordArray.map((item, index) => {
-    return `<li>
-              <span>${index + 1}</span>  <span>${item}</span>
-           </li>`;
-  }).join('');
-  template += `<li><span>1</span>  <span>${this.rankKeywordArray[0]}</span></li>`
+SearchTermRankView.prototype.requestJsonp = function (word, callbackName) {
+  const script = document.createElement('script');
+  script.src = `${this.currentTypingUrl}&q=${word}&callback=${callbackName}`;
+  document.body.append(script);
+}
 
-  this.$rankKeywordList.innerHTML = template;
+SearchTermRankView.prototype.putInResponseJsonpData = function (data) {
+  this.currentTypingArray = data.items.map(item => item.replace(/\|.+/g, ''));
+}
+
+SearchTermRankView.prototype.focusInputHandler = function () {
+  this.$rankKeywordList.classList.add('display_none');
+  if (!this.$inputElement.value) _.$(`.${this.inputPopupClassName}`).classList.remove('display_none');
+  if (this.currentTypingArray.length) this.$searchWord.classList.remove('display_none');
+  clearTimeout(this.rolling.timer);
+  this.rolling.timer = null;
+}
+
+SearchTermRankView.prototype.typingInputHandler = function ({ target: { value } }) {
+  this.keyWordIndex = -1;
+  if (this.typingTimer) clearTimeout(this.typingTimer);
+  this.typingTimer = setTimeout(async function () {
+    await delay(this.requestJsonp(value, 'putInResponseJsonpData'), 500);
+    if (value) {
+      if (this.currentTypingArray.length) this.$searchWord.classList.remove('display_none');
+      _.$(`.${this.inputPopupClassName}`).classList.add('display_none');
+      this.renderSearchWordTemplate(value);
+    } else {
+      this.$searchWord.classList.add('display_none');
+      _.$(`.${this.inputPopupClassName}`).classList.remove('display_none');
+    }
+  }.bind(this), 500);
+}
+
+SearchTermRankView.prototype.quitInputHandler = function ({ target }) {
+  if (target === this.$inputElement || target.closest('.head__search--input')) return;
+  _.$(`.${this.inputPopupClassName}`).classList.add('display_none');
+  this.$searchWord.classList.add('display_none');
+  if (this.$inputElement.value) return;
+  this.$rankKeywordList.classList.remove('display_none');
+  if (!this.rolling.timer) this.rollingSearchBar();
+}
+
+SearchTermRankView.prototype.keydownInputHandler = function ({ key }) {
+  if (this.$searchWord.classList.contains('display_none')) return;
+  if (!(key === 'ArrowDown' || key === 'ArrowUp')) return;
+  const keyWordList = _.$All(`.${this.$searchWord.className} > ul > li`);
+  keyWordList.forEach((element) => element.classList.remove('gray_background'));
+  switch (key) {
+    case 'ArrowDown':
+      this.keyWordIndex = this.keyWordIndex < keyWordList.length - 1 ? ++this.keyWordIndex : this.keyWordIndex;
+      break;
+    case 'ArrowUp':
+      this.keyWordIndex = this.keyWordIndex > 0 ? --this.keyWordIndex : this.keyWordIndex;
+      break;
+  }
+  keyWordList[this.keyWordIndex].classList.add('gray_background');
+  this.$inputElement.value = keyWordList[this.keyWordIndex].innerText;
 }
 
 SearchTermRankView.prototype.rollingSearchBar = function () {
@@ -59,13 +112,47 @@ SearchTermRankView.prototype.observeSearchBarState = function () {
       await delay('', 1000);
       this.$rankKeywordList.classList.add('transition_on');
     }
-  })
+  });
   const config = { attributes: true };
   observer.observe(this.$rankKeywordList, config);
 }
 
+SearchTermRankView.prototype.renderRollingTemplate = function () {
+  let template = this.rankKeywordArray.map((item, index) => {
+    return `<li>
+              <span>${index + 1}</span>  <span>${item}</span>
+           </li>`;
+  }).join('');
+  template += `<li><span>1</span>  <span>${this.rankKeywordArray[0]}</span></li>`;
 
+  this.$rankKeywordList.innerHTML = template;
+}
 
+SearchTermRankView.prototype.renderPopupTemplate = function () {
+  const template = `<div class="${this.inputPopupClassName} display_none">
+                      <div>인기 쇼핑 키워드</div>
+                      <ul>
+                        ${this.rankKeywordArray.map((item, index) => {
+    return `<li>
+             <span class="bold">${index + 1}</span> <span>${item}</span>
+            </li>`
+  }).join('')}
+                      </ul>
+                    </div>`;
 
+  this.$inputElement.insertAdjacentHTML('afterend', template);
+}
+
+SearchTermRankView.prototype.renderSearchWordTemplate = function (word) {
+  const template = `<ul>
+                      ${this.currentTypingArray.map(item => {
+    return `<li>
+              <span>${item.replace(word, `<span class='highlight'>${word}</span>`)}</span>
+            </li>`
+  }).join('')}
+                    </ul>`;
+
+  this.$searchWord.innerHTML = template;
+}
 
 export default SearchTermRankView;
--- shoppingHow/public/src/main.js
+++ shoppingHow/public/src/main.js
@@ -36,15 +36,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const bottomCarouselView = new BottomCarouselView({ loadDataManager }, bottomElements);
 
-
   const searchInput = {
     $inputElement: _.$('._search_input'),
     $rankKeywordList: _.$('._rank_keyword_list'),
+    $searchWord: _.$('.head__search--input--word'),
     rankUrl: 'https://shoppinghow.kakao.com/v1.0/shophow/top/recomKeyword.json?_=1615189485140',
-
+    currentTypingUrl: 'https://suggest-bar.daum.net/suggest?limit=10&mode=json&code=utf_in_out&id=shoppinghow_suggest',
+    rolling: {
+      timer: null,
+      timeSeconds: 3000,
+      maxPosition: '220'
+    }
   }
 
   const searchTermRankView = new SearchTermRankView(searchInput);
-
 
 })
