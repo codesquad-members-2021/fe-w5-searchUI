234f3a275edb0c05c8291cca7f9c9f06d4eee6f1
Hongbi <dyongdi@gmail.com>
2021-03-12 15:41:36 +0900

    Add. util 폴더 생성, 필요한 url을 객체에 담아서 사용

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,18 @@
-import { _ } from './util.js';
-import { $search, $rollingKeywords, $recommendedKeyword } from './ref.js';
+import { _ } from './util/util.js';
+import { $search, $rollingKeywords, $recommendedKeyword } from './util/ref.js';
+import { URL, KEYCODE } from './util/data.js';
 const { log } = console;
 
 $search.addEventListener('input', () => {
   const searchInput = $search.value;
-  fetch(`
-  https://completion.amazon.com/api/2017/suggestions?session-id=146-7463946-6351015&customer-id=&request-id=GJ3692SXTPG2EG7P3KFS&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=undefined&prefix=${searchInput}&event=onFocusWithSearchTerm&limit=11&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1615392526147
-  `)
+  fetch(URL.autoComplete(searchInput))
     .then(res => res.json())
     .then(jsonData => jsonData.suggestions.map(v => v.value))
     .then(console.log);
 })
 
 const renderRecommendedKeywords = async () => {
-  const recommendedKeywords = await fetch(`
-    https://shoppinghow.kakao.com/v1.0/shophow/top/recomKeyword.json
-  `)
+  const recommendedKeywords = await fetch(URL.RECOMMEND)
     .then(res => res.json())
     .then(jsonData => jsonData.list.map(v => v.keyword));
 
---
+++ public/js/util/data.js
@@ -0,0 +1,31 @@
+// 주석 처리 부분은 카일 코드의 상황.. 나중에 내꺼에 맞게 바꿔야 함
+
+// const CLASS_LIST = {
+//   HIDDEN: 'hidden',
+//   CURRENT_PAGE: 'current-page',
+//   PREV_BTN: 'btn-prev',
+//   NEXT_BTN: 'btn-next',
+//   PREV_ICON: 'fa-chevron-left',
+//   NEXT_ICON: 'fa-chevron-right',
+//   MORE_LIST: 'event-item-list',
+//   PLACEHOLDER_ITEM: 'placeholder-item',
+//   SEARCH_TAB_LIST: 'search-tab__list',
+//   AUTOCOMPLETE_LIST: 'auto-complete__list',
+//   AUTOCOMPLETE_ITEM: 'auto-complete__item',
+// };
+
+const URL = {
+  // MORE: 'http://localhost:8080/moreItem',
+  // SLIDE: 'https://shoppinghow.kakao.com/v1.0/shophow/top/planningEvent.json?_=1614221190473',
+  RECOMMEND: 'https://shoppinghow.kakao.com/v1.0/shophow/top/recomKeyword.json',
+  autoComplete: function (keyword) {
+    return `https://completion.amazon.com/api/2017/suggestions?session-id=136-7196080-8943745&customer-id=&request-id=64K2NCH8DSXF305XB5GB&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=78&prefix=${keyword}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD&suggestion-type=WIDGET&_=1615428578330`;
+  },
+};
+
+const KEYCODE = {
+  UP: 38,
+  DOWN: 40,
+};
+
+export { URL, KEYCODE }
