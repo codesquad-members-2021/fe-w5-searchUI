faf56638131e1cbb9903f422a1182182ea66f734
swing-park <ongsim123@gmail.com>
2021-03-14 17:10:38 +0900

    Add : directionKeyEventHandler function

--- public/js/searchDropdownUI.js
+++ public/js/searchDropdownUI.js
@@ -1,26 +1,25 @@
-import { _ } from "./util";
+import { _, setStyle } from "./util";
 import RollingUI from "./rollingUI";
 
 export class SearchDropDownUI extends RollingUI {
     constructor(el) {
         super(el);
         this.timer;
-        this.autoCompleteData;
+        this.suggestions;
+        this.defaultInputValue;
         this.inputSelector = _.$("#search--input");
         this.autoCompleteUI = _.$("#input--autoCompleteUI");
         this.autoCompleteLists = _.$("#autoCompleteLists");
-        this.directionKeyCnt = 0;
+        this.directionKeyCnt = -1;
     }
 }
 
 SearchDropDownUI.prototype.eventHandler = async function (el) {
     _.on(el, "click", this.focusHandler.bind(this));
     _.on(el, "blur", this.blurHandler.bind(this));
-    _.on(el, "mouseout", this.mouseoutHandler.bind(this));
-    _.on(el, "mouseover", this.mouseoverHandler.bind(this));
-    _.on(el, "keyup", this.keyupHandler.bind(this, this.inputSelector));
-    _.on(el, "keyup", this.autoCompleteInit.bind(this, this.autoCompleteLists));
-    _.on(el, "keyup", this.directionKeyEventHandler.bind(this));
+    _.on(el.parentNode, "mouseout", this.mouseoutHandler.bind(this));
+    _.on(el.parentNode, "mouseover", this.mouseoverHandler.bind(this));
+    _.on(el, "keyup", this.keyupHandler.bind(this));
 };
 
 SearchDropDownUI.prototype.focusHandler = function () {
@@ -87,7 +86,13 @@ SearchDropDownUI.prototype.makeLists = function (data) {
     return innerHTML
 };
 
-SearchDropDownUI.prototype.keyupHandler = function (inputSelector) {
+SearchDropDownUI.prototype.keyupHandler = async function ({ keyCode }) {
+    await this.pickSeenScreen(this.inputSelector);
+    await this.autoCompleteInit(this.autoCompleteLists, keyCode);
+    this.directionKeyEventHandler(keyCode);
+}
+
+SearchDropDownUI.prototype.pickSeenScreen = function (inputSelector) {
     if (inputSelector.value.length === 0) {
         this.el.style.display = "block";
         _.$("#input--autoCompleteUI").style.display = "none";
@@ -95,9 +100,11 @@ SearchDropDownUI.prototype.keyupHandler = function (inputSelector) {
     }
     this.el.style.display = "none";
     _.$("#input--autoCompleteUI").style.display = "block";
-}
+};
 
-SearchDropDownUI.prototype.autoCompleteInit = function (el) {
+SearchDropDownUI.prototype.autoCompleteInit = function (el, keyCode) {
+    if (keyCode === 38 || keyCode === 40) return
+    this.defaultInputValue = this.inputSelector.value;
     const makeAutoCompleteData = async () => {
         let url = `https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${this.inputSelector.value}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`
         const data = await this.autoCompleteLoadData(url);
@@ -114,7 +121,7 @@ SearchDropDownUI.prototype.autoCompleteLoadData = async function (url) {
     const data = await fetch(url);
     const json = await data.json();
     const suggestions = json.suggestions.map((v) => v.value);
-    this.autoCompleteData = suggestions;
+    this.suggestions = suggestions;
     return suggestions.length === 0 ? "" : suggestions
 };
 
@@ -139,10 +146,42 @@ SearchDropDownUI.prototype.debouncer = function (fn, ms) {
 };
 
 SearchDropDownUI.prototype.giveHighlight = function (lists) {
+    if (lists.length === 0) return ""
     let highlightedLists = [];
     lists.map((idx) => {
         let regex = new RegExp(this.inputSelector.value, "g");
         highlightedLists.push(idx.replace(regex, `<span class='highlight'>${this.inputSelector.value}</span>`))
     })
     return highlightedLists
+};
+
+SearchDropDownUI.prototype.directionKeyEventHandler = function (keyCode) {
+    const lists = this.autoCompleteLists.childNodes;
+
+    lists.forEach(list => _.removeClass("background", list));
+    if (keyCode === 38) {
+        this.directionKeyCnt--;
+        if (this.directionKeyCnt <= -1) {
+            this.inputSelector.value = this.defaultInputValue;
+            this.directionKeyCnt = -1;
+            setStyle({ el: this.autoCompleteUI, styleRef: { opacity: "0" } })
+            return
+        }
+    }
+    else if (keyCode === 40) {
+        this.directionKeyCnt++;
+        if (this.directionKeyCnt >= lists.length) {
+            this.inputSelector.value = this.defaultInputValue;
+            this.directionKeyCnt = lists.length;
+            setStyle({ el: this.autoCompleteUI, styleRef: { opacity: "0" } })
+            return
+        }
+    }
+    setStyle({ el: this.autoCompleteUI, styleRef: { opacity: "1" } })
+    _.addClass("background", lists[this.directionKeyCnt]);
+    this.inputValueHandler(this.directionKeyCnt)
+};
+
+SearchDropDownUI.prototype.inputValueHandler = function (cnt) {
+    this.inputSelector.value = this.suggestions[cnt];
 };
