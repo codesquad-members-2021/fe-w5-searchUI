0ea60e062811392d76e5d6369510ffbddfc31192
swing-park <ongsim123@gmail.com>
2021-03-14 17:07:37 +0900

    Add : addClass , removeClass function

--- public/js/util.js
+++ public/js/util.js
@@ -1,6 +1,8 @@
 const _ = {
     $: (selector, base = document) => base.querySelector(selector),
     $All: (selector, base = document) => base.querySelectorAll(selector),
+    addClass: (className, target) => target.classList.add(className),
+    removeClass: (className, target) => target.classList.remove(className),
     on: (el, event, fn) => el.addEventListener(event, fn)
 };
 
