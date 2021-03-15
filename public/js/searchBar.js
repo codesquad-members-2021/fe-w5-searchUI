c52b2613e7dc18d658cedd78c6d59efb65ae0b82
Autumn (조홍비) <60209518+dyongdi@users.noreply.github.com>
2021-03-16 01:15:42 +0900

    [Autumn] 인기 검색어 롤링 구현 (#84)
    * Add. rolling keywords 데이터 화면에 표시 (아직 rolling 되지는 않음)
    
    - 키워드 번호와 함께 표시되도록 했음
    - 쇼핑하우에는 10번까지만 나오는데 일단은 받아온 데이터 모두 화면에 표시
    - 번호와 함께 표시하기 위해 span태그도 생성하도록 변경
    
    * Feat. 인기 검색어 롤링 구현
    
    - 동작만 되도록 막 짜서 코드가 엉망
    - 1번 아이템에서 오랜 시간 머무는 문제 고쳐야 함
    - 10번 -> 1번 가자마자 ol의 위치를 변경해줘야 함

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -1,6 +1,7 @@
 import { URL } from './util/data.js';
-import { $searchBar, $searchBarInput, $searchSuggestions, $rollingKeywords, $recommendedKeyword } from './util/ref.js';
+import { $searchBar, $searchBarInput, $searchSuggestions, $rollingKeywords } from './util/ref.js';
 import { CLASS_LIST } from './util/cssClasses.js';
+import { delay } from './util/util.js';
 
 class SearchBar {
   constructor() {
@@ -25,11 +26,57 @@ class SearchBar {
 
 class RollingUI {
   constructor() {
+    // constructor에서는 직접적으로 async, await 사용이 불가능하니,
+    // constructor에서 init 함수를 호출하고,
+    // init 함수에서 변수를 정의해주자.
+    this.rollingListUI = $rollingKeywords;
+    this.currItemIdx = 0;
+    this.currY = 4;
+    this.heightOfOneItem = 30;
+    this.init();
+  }
 
+  async getKeywords() {
+    const recommendedKeywords = await fetch(URL.RECOMMEND)
+      .then(res => res.json())
+      .then(jsonData => jsonData.list.map(v => v.keyword).slice(0, 10));
+    return recommendedKeywords
   }
 
   render() {
+    let renderingValue = this.keywords.reduce((prev, curr, idx) => {
+      return prev + `<li><span class="num-rank">${idx + 1}</span>${this.keywords[idx]}</li>`
+    }, '')
+    renderingValue += `<li><span class="num-rank">1</span>${this.keywords[0]}</li>`
+    this.rollingListUI.innerHTML = renderingValue;
+  }
 
+  async roll() {
+    await delay('', 1000);
+    const rollngTimeoutId = setTimeout(() => {
+      this.setRollingAnimation();
+      this.currItemIdx++;
+      if(this.currItemIdx === 11) {
+        this.currY = 4;
+        this.rollingListUI.style.transform = '';
+        this.rollingListUI.style.transition = '';
+        this.currItemIdx = 0;
+      }
+      this.roll()
+    }, 1000);
+  }
+
+  setRollingAnimation() {
+    this.rollingListUI.style.transform = `translateY(${this.currY - this.heightOfOneItem}px)`;
+    this.rollingListUI.style.transition = 'all 1s'
+    this.currY -= this.heightOfOneItem;
+    console.log('애니메이션!');
+  }
+
+  async init(){
+    this.keywords = await this.getKeywords();
+    this.render();
+    this.roll();
   }
 }
 
@@ -46,15 +93,8 @@ class KeywordSuggestion {
     const renderedValue = recommendedKeywords.reduce((prev, curr, idx) => {
       return prev + `<li>${recommendedKeywords[idx]}</li>`
     }, '<ol>')
-    $recommendedKeyword.innerHTML = renderedValue + '</ol>';
+    $searchSuggestions.innerHTML = renderedValue + '</ol>';
   }
 }
 
-const init = () => {
-  const searchBar = new SearchBar();
-  searchBar.registerEvent();
-  const keywordSuggestion = new KeywordSuggestion();
-  keywordSuggestion.render();
-}
-
-export default init
+export { SearchBar, RollingUI, KeywordSuggestion }
