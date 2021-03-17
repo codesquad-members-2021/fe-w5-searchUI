196543fd4f3fbecab5e70d1f5da31e596a527c7b
Hongbi <dyongdi@gmail.com>
2021-03-17 18:44:28 +0900

    Add. util debounce 함수 추가

--- public/js/util/util.js
+++ public/js/util/util.js
@@ -7,5 +7,16 @@ const pipe = (...fns) => arg => fns.reduce((arg, fn) => fn(arg), arg);
 const createDom = (tag) => ({ value, classes }) => `<${tag} class='${classes.join(' ')}'>${value}</${tag}>`;
 const getData = (url) => fetch(url).then((res) => res.json());
 const delay = (value, ms) => new Promise((resolve) => setTimeout(() => resolve(value), ms));
+const debounce = (fn, ms) => {
+  let timeoutID;
+  return function(...args) {
+    if(timeoutID) {
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {
+      fn(...args)
+    }, ms)
+  }
+}
 
-export { _, pipe, createDom, getData, delay }
+export { _, pipe, createDom, getData, delay, debounce }
